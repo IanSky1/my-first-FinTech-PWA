9e15c3a569ed030020532450fec03bc5a12b8f55
Ian <Ian.Mohn1@gmail.com>
2022-07-10 19:58:15 -0500

    Created .gitignore file

---
+++ public/js/idb.js
@@ -0,0 +1,11 @@
+let db;
+
+const request = indexedDB.open('budget_tracker', 1);
+
+request.onupgradeneeded = function(event) {
+
+    const db = event.target.result;
+
+    db.CreateObjectStore('new_transaction', { autoIncrement: true });
+};
+
